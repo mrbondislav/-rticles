21be8cefd589f63fc27791dce404865414368192
mrbondislav <vlad.fedoseev1@mail.ru>
2022-08-29 19:52:07 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from "@mui/material/Container";
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Header } from "./components";
+import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+import React from "react";
+import { fetchAuthMe, selectIsAuth } from "./redux/slices/auth";
 
 function App() {
+  const dispatch = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
+
+  React.useEffect(() => {
+    dispatch(fetchAuthMe());
+  }, []);
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Container maxWidth="lg">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/posts/:id" element={<FullPost />} />
+          <Route path="/posts/:id/edit" element={<AddPost />} />
+          <Route path="/add-post" element={<AddPost />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Registration />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+
+import styles from './Header.module.scss';
+import Container from '@mui/material/Container';
+import { useSelector } from 'react-redux';
+import { logout, selectIsAuth } from '../../redux/slices/auth';
+
+export const Header = () => {
+  const dispath = useDispatch();
+  const isAuth = useSelector(selectIsAuth);
+
+  const onClickLogout = () => {
+    if (window.confirm('Уже уходите?')) {
+      dispath(logout());
+      window.localStorage.removeItem('token');
+    }
+  };
+
+  return (
+    <div className={styles.root}>
+      <Container maxWidth="lg">
+        <div className={styles.inner}>
+          <Link to={"/"} className={styles.logo}>
+            <div>Fedoseev BLOG</div>
+          </Link>
+          <div className={styles.buttons}>
+            {isAuth ? (
+              <>
+                <Link to="/add-post">
+                  <Button variant="contained">Написать статью</Button>
+                </Link>
+                <Button onClick={onClickLogout} variant="contained" color="error">
+                  Выйти
+                </Button>
+              </>
+            ) : (
+              <>
+                <Link to="/login">
+                  <Button variant="outlined">Войти</Button>
+                </Link>
+                <Link to="/register">
+                  <Button variant="contained">Создать аккаунт</Button>
+                </Link>
+              </>
+            )}
+          </div>
+        </div>
+      </Container>
+    </div>
+  );
+};
